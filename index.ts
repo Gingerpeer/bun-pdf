160d393bd7152840c8f3ee5a699f0b0b756aab1d
Pierre Daniel van Zyl <pierrevanzyl777@gmail.com>
2024-03-04 16:44:37 +0200

    creating an array of strings without new lines after every word and removing all extra spacing

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
-import { pdfToText, pdfToPages } from 'pdf-ts';
+import { /* pdfToText , */ pdfToPages } from 'pdf-ts';
 
 const pdf = Bun.file('./pdf/Resume P. D. van Zyl.pdf');
 const arrayBuffer = await pdf.arrayBuffer();
 const normalBuffer = await Buffer.from(arrayBuffer);
-const text = await pdfToText(normalBuffer);
 const pages = await pdfToPages(normalBuffer);
 
-console.log(pages.map((page)=> page.text.replaceAll('\n', ' ')));
+const textArray: string[] | undefined = [];
+
+pages.map((page)=> textArray.push(page.text.replaceAll('\n', ' ').replaceAll('  ', ' ')));
+
+console.log(textArray);
 
 
