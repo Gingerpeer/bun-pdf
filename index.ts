969897c64fd942a3ece81e9676392e3d03058f34
Pierre Daniel van Zyl <pierrevanzyl777@gmail.com>
2024-03-04 12:35:07 +0200

    working with pdf and getting text

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
+import { pdfToText, pdfToPages } from 'pdf-ts';
+
+const pdf = Bun.file('./pdf/Resume P. D. van Zyl.pdf');
+const arrayBuffer = await pdf.arrayBuffer();
+const normalBuffer = await Buffer.from(arrayBuffer);
+const text = await pdfToText(normalBuffer);
+const pages = await pdfToPages(normalBuffer);
+
+console.log(pages.map((page)=> page.text.replaceAll('\n', ' ')));
 
 
-
-console.log("Hello via Bun!");
