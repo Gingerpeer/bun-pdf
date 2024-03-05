cbaee24a84ca49c4a89d98a7659406cd7203e429
Pierre Daniel van Zyl <pierrevanzyl777@gmail.com>
2024-03-05 23:33:54 +0200

    copy to clipboard done using clipboardy

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import { /* pdfToText , */ pdfToPages } from 'pdf-ts';
-import OpenAI from "openai";
+import clipboard from 'clipboardy';
+// import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: Bun?.env.openAi
-});
+// const openai = new OpenAI({
+//   apiKey: Bun?.env.openAi
+// });
 
-const fileName = 'Ch15_v02'
+const fileName = 'ch5'
 
 const pdf = Bun.file(`./pdf/${fileName}.pdf`);
 const arrayBuffer = await pdf.arrayBuffer();
@@ -16,16 +17,23 @@ const textArray: string[] | undefined = [];
 
 pages.map((page)=> textArray.push(page.text.replaceAll('\n', '')));
 
-await Bun.write(`./output/${fileName}.txt`, textArray.join("\n"));
+// await navigator.clipboard.writeText(text);
+copyToClipboard(textArray[1]);
+console.log(textArray[1]);
+async function copyToClipboard(text: string) {
+  clipboard.writeSync(text);
+  clipboard.readSync();
+}
+// await Bun.write(`./output/${fileName}.txt`, textArray.join("\n"));
 // console.log(textArray);
 
-async function prompt() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
+// async function prompt() {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: "system", content: "You are a helpful assistant." }],
+//     model: "gpt-3.5-turbo",
+//   });
 
-  console.log(completion.choices[0]);
-}
+//   console.log(completion.choices[0]);
+// }
 
 // prompt()
