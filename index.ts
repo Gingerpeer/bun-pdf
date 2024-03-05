494c4f399dee0b0e02678d4866c79d4f014057ce
Pierre Daniel van Zyl <pierrevanzyl777@gmail.com>
2024-03-05 12:03:16 +0200

    trying to add openai to summarize pdfs

--- index.ts
+++ index.ts
@@ -1,14 +1,31 @@
 import { /* pdfToText , */ pdfToPages } from 'pdf-ts';
+import OpenAI from "openai";
 
-const pdf = Bun.file('./pdf/Resume P. D. van Zyl.pdf');
+const openai = new OpenAI({
+  apiKey: Bun?.env.openAi
+});
+
+const fileName = 'Ch15_v02'
+
+const pdf = Bun.file(`./pdf/${fileName}.pdf`);
 const arrayBuffer = await pdf.arrayBuffer();
 const normalBuffer = await Buffer.from(arrayBuffer);
 const pages = await pdfToPages(normalBuffer);
 
 const textArray: string[] | undefined = [];
 
-pages.map((page)=> textArray.push(page.text.replaceAll('\n', ' ').replaceAll('  ', ' ')));
+pages.map((page)=> textArray.push(page.text.replaceAll('\n', '')));
 
-console.log(textArray);
+await Bun.write(`./output/${fileName}.txt`, textArray.join("\n"));
+// console.log(textArray);
 
+async function prompt() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  });
 
+  console.log(completion.choices[0]);
+}
+
+// prompt()
